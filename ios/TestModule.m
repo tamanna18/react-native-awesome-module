dabbaedd70c15e0cffd4302970ff71358252f2ed
pulkit604 <pulkit.gupta@stud.hs-ruhrwest.de>
2022-11-10 00:37:05 +0100

    ios test library

--- ios/TestModule.m
+++ ios/TestModule.m
@@ -6,6 +6,6 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(NavigateMe: )
+RCT_EXTERN_METHOD(NavigateMe: (void))
 
 @end
